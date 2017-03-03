699c2a427f8aa5b3766bd3c1a389e2d22b7a60a9
Roman Rozhkov <rrozhkov@luxoft.com>
2017-03-03 15:59:37 +0600

    Refactoring

--- app/src/main/java/ru/rrozhkov/easykin/android/ws/client/EasyKinService.java
+++ app/src/main/java/ru/rrozhkov/easykin/android/ws/client/EasyKinService.java
@@ -9,7 +9,6 @@ import ru.rrozhkov.easykin.android.ws.client.process.impl.ProcessRunner;
 import ru.rrozhkov.easykin.android.ws.client.process.impl.TaskProcessor;
 import ru.rrozhkov.easykin.model.category.ICategory;
 import ru.rrozhkov.easykin.model.fin.payment.IPayment;
-import ru.rrozhkov.easykin.model.person.IPerson;
 import ru.rrozhkov.easykin.model.task.ITask;
 import ru.rrozhkov.lib.collection.CollectionUtil;
 import ru.rrozhkov.lib.ws.process.IProcessor;
