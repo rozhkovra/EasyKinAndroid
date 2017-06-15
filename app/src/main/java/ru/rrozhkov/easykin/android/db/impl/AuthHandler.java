d733f4fa77408903f318a04f98eca877e4cf38ac
lux_rozhkov <rrozhkov@luxoft.com>
2017-06-15 14:01:23 +0600

    EKINA-0017 Add another crypter for android

--- app/src/main/java/ru/rrozhkov/easykin/android/db/impl/AuthHandler.java
+++ app/src/main/java/ru/rrozhkov/easykin/android/db/impl/AuthHandler.java
@@ -5,8 +5,8 @@ import java.util.Collection;
 
 import ru.rrozhkov.easykin.android.model.person.impl.convert.DBPersonConverter;
 import ru.rrozhkov.easykin.model.person.IPerson;
+import ru.rrozhkov.lib.android.crypt.AndroidEncrypter;
 import ru.rrozhkov.lib.collection.CollectionUtil;
-import ru.rrozhkov.lib.crypt.Encrypter;
 
 /**
  * Created by rrozhkov on 6/5/2017.
@@ -16,7 +16,7 @@ public class AuthHandler {
     private static String select = "select * from person where username='#username#' and password='#password#'";
     public static IPerson auth(String username, String password) throws SQLException {
         Collection<IPerson> persons = EasyKinDBManager.instance().select(
-                select.replace("#username#",username).replace("#password#", Encrypter.encrypt(password))
+                select.replace("#username#",username).replace("#password#", AndroidEncrypter.encrypt(password))
                 , new DBPersonConverter());
         if(persons.isEmpty() || persons.size()>1)
             return null;
---
+++ app/src/main/java/ru/rrozhkov/lib/android/crypt/AndroidEncrypter.java
@@ -0,0 +1,36 @@
+package ru.rrozhkov.lib.android.crypt;
+
+import android.util.Base64;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by rrozhkov on 6/15/2017.
+ */
+
+public class AndroidEncrypter {
+    public static String encrypt(String text){
+        MessageDigest md = null;
+        try{
+            md = MessageDigest.getInstance("SHA");
+        }catch(NoSuchAlgorithmException e){
+            e.printStackTrace();
+        }
+
+        try {
+            md.update(text.getBytes("UTF-8"));
+        }catch (UnsupportedEncodingException e){
+            e.printStackTrace();
+        }
+        byte raw[] = md.digest();
+        String hash = new String(Base64.encode(raw, Base64.DEFAULT)); //step 5
+        return hash;
+    }
+
+    public static void main(String[] args) {
+        String password = "123";
+        System.out.println(AndroidEncrypter.encrypt(password));
+    }
+}
