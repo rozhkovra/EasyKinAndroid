8b99fbc907476160f102dd1581cb9e1fd2dde40d
lux_rozhkov <rrozhkov@luxoft.com>
2017-06-27 18:20:14 +0600

    EKINA-0020 Task list order change

--- app/src/main/java/ru/rrozhkov/easykin/android/db/impl/TaskHandler.java
+++ app/src/main/java/ru/rrozhkov/easykin/android/db/impl/TaskHandler.java
@@ -3,6 +3,9 @@ package ru.rrozhkov.easykin.android.db.impl;
 import android.content.ContentValues;
 import android.database.Cursor;
 
+import java.sql.SQLException;
+import java.util.Collection;
+
 import ru.rrozhkov.easykin.android.model.task.impl.convert.DBTaskConverter;
 import ru.rrozhkov.easykin.android.model.task.impl.convert.TaskContentConverter;
 import ru.rrozhkov.easykin.model.task.ITask;
@@ -14,6 +17,13 @@ import ru.rrozhkov.lib.convert.IConverter;
  */
 
 public class TaskHandler extends EntityHandler<ITask>{
+    public String select = "SELECT "+tableName()+".*, category.name as categoryName FROM "+tableName()
+            +" INNER JOIN category on "+tableName()+".category = category.id"
+            +" ORDER BY status, priority, category";
+
+    public Collection<ITask> select() throws SQLException {
+        return EasyKinDBManager.instance().select(select, dbConverter());
+    }
     @Override
     public String tableName() {
         return "task";
