23afb9972df1a1f583a1283bc171b5121829a509
Roman Rozhkov <rrozhkov@luxoft.com>
2017-08-31 15:50:41 +0600

    EKINA-0027 Add payments

---
+++ app/src/main/java/ru/rrozhkov/easykin/android/db/impl/PaymentHandler.java
@@ -0,0 +1,31 @@
+package ru.rrozhkov.easykin.android.db.impl;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+
+import ru.rrozhkov.easykin.android.model.payment.impl.convert.DBPaymentConverter;
+import ru.rrozhkov.easykin.android.model.payment.impl.convert.PaymentContentConverter;
+import ru.rrozhkov.easykin.model.fin.payment.IPayment;
+import ru.rrozhkov.lib.android.db.impl.EntityHandler;
+import ru.rrozhkov.lib.convert.IConverter;
+
+/**
+ * Created by rrozhkov on 6/1/2017.
+ */
+
+public class PaymentHandler extends EntityHandler<IPayment>{
+    @Override
+    public String tableName() {
+        return "payment";
+    }
+
+    @Override
+    public IConverter<IPayment, ContentValues> converter() {
+        return new PaymentContentConverter();
+    }
+
+    @Override
+    public IConverter<Cursor, IPayment> dbConverter() {
+        return new DBPaymentConverter();
+    }
+}
---
+++ app/src/main/java/ru/rrozhkov/easykin/android/model/payment/impl/convert/PaymentContentConverter.java
@@ -0,0 +1,25 @@
+package ru.rrozhkov.easykin.android.model.payment.impl.convert;
+
+import android.content.ContentValues;
+
+import ru.rrozhkov.easykin.model.fin.payment.IPayment;
+import ru.rrozhkov.easykin.model.fin.payment.PaymentCategory;
+import ru.rrozhkov.easykin.model.fin.payment.PaymentStatus;
+import ru.rrozhkov.lib.convert.IConverter;
+
+/**
+ * Created by rrozhkov on 5/30/2017.
+ */
+
+public class PaymentContentConverter implements IConverter<IPayment, ContentValues> {
+    @Override
+    public ContentValues convert(IPayment payment) {
+        ContentValues map = new ContentValues();
+        map.put("id", payment.getId());
+        map.put("comment", payment.getComment());
+        map.put("category", PaymentCategory.category(payment.getCategory()));
+        map.put("status", PaymentStatus.status(payment.getStatus()));
+        map.put("amount", payment.getAmount().getValue());
+        return map;
+    }
+}
