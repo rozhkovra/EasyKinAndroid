b3f2f634a0975823173ec75b3f48e88e34fc159b
Roman Rozhkov <rrozhkov@luxoft.com>
2018-09-10 17:18:24 +0600

    EKIN-0264 Show finance in list

--- app/src/main/java/ru/rrozhkov/easykin/android/fin/FinanceListActivity.java
+++ app/src/main/java/ru/rrozhkov/easykin/android/fin/FinanceListActivity.java
@@ -1,19 +1,33 @@
 package ru.rrozhkov.easykin.android.fin;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.Collection;
 
 import ru.rrozhkov.easykin.android.R;
+import ru.rrozhkov.easykin.android.context.MasterDataContext;
+import ru.rrozhkov.easykin.android.context.impl.MasterDataContextFactory;
+import ru.rrozhkov.easykin.android.model.payment.impl.convert.PaymentArrayConverter;
+import ru.rrozhkov.easykin.model.fin.payment.IPayment;
 
 public class FinanceListActivity extends AppCompatActivity {
+    private ListView listView;
+    private MasterDataContext context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        new ManageDataTask().execute((Void) null);
+
         setContentView(R.layout.activity_finance_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -26,5 +40,31 @@ public class FinanceListActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private void rereshFinanceList() {
+        Collection<IPayment> payments = context.finance();
+        ArrayAdapter<String> adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, new PaymentArrayConverter().convert(payments));
+        listView = (ListView) findViewById(R.id.financeList);
+        listView.setAdapter(adapter);
+    }
+
+    private void refresh(){
+        rereshFinanceList();
+        Toast.makeText(this.getBaseContext(),"Payments was reloaded!",Toast.LENGTH_SHORT).show();
+    }
+    public class ManageDataTask extends AsyncTask<Void, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            FinanceListActivity.this.context = MasterDataContextFactory.instance(false);
+            FinanceListActivity.this.context.init();
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            FinanceListActivity.this.refresh();
+        }
     }
 }
